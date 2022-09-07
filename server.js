43587e17134cbeb7de5549c4d481b14707e000ad
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-07 17:31:59 +0100

    created new repo and copied over my old code for app

---
+++ server.js
@@ -0,0 +1,26 @@
+// required modules
+const express = require("express");
+
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+// express server create at server port 3000
+const app = express();
+const PORT = process.env.PORT || 3007;
+
+// parse date from string or array
+app.use(express.urlencoded({ extended: true }));
+// parse json data
+app.use(express.json());
+// use public folder
+app.use(express.static("public"));
+
+// routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+
+// event listener
+app.listen(PORT, () => {
+  console.log(`Now listening on PORT: ${PORT}`);
+});
